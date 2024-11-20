a996bc319c8cc717f269bfa4554a361949b6e633
tinasgong <tinagong@college.harvard.edu>
2024-11-20 14:50:55 -0500

    tina changes from monday

--- train_segmentation.py
+++ train_segmentation.py
@@ -12,6 +12,27 @@ from utils.error_logger import ErrorLogger
 
 from models import get_model
 
+from torchvision.datasets import Cityscapes
+from torchvision.transforms import Compose, ToTensor, Normalize, Resize
+
+def get_cityscapes_dataset(data_path, split, transform):
+    return Cityscapes(
+        root=data_path,
+        split=split,
+        mode='fine',
+        target_type='semantic',
+        transform=transform,
+        target_transform=transform
+    )
+
+def get_cityscapes_transforms():
+    return Compose([
+        Resize((512, 1024)),  # Resize images to a smaller resolution
+        ToTensor(),          # Convert to PyTorch tensor
+        Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize for pretrained models
+    ])
+
+
 def train(arguments):
     # Parse input arguments
     json_filename = arguments.config
@@ -30,20 +51,32 @@ def train(arguments):
     ds_transform = get_dataset_transformation(arch_type, opts=json_opts.augmentation)
 
     # Setup the NN Model
-    # model = get_model(json_opts.model)
-    # if network_debug:
-    #     print('# of pars: ', model.get_number_parameters())
-    #     print('fp time: {0:.3f} sec\tbp time: {1:.3f} sec per sample'.format(*model.get_fp_bp_time2()))
-    #     exit()
+    model = get_model(json_opts.model)
+    if network_debug:
+        print('# of pars: ', model.get_number_parameters())
+        print('fp time: {0:.3f} sec\tbp time: {1:.3f} sec per sample'.format(*model.get_fp_bp_time2()))
+        exit()
 
     # Setup Data Loader
-    train_dataset = ds_class(ds_path, split='train',      transform=ds_transform['train'], preload_data=train_opts.preloadData)
-    valid_dataset = ds_class(ds_path, split='val',        transform=ds_transform['valid'], preload_data=train_opts.preloadData)
-    test_dataset  = ds_class(ds_path, split='test',       transform=ds_transform['valid'], preload_data=train_opts.preloadData)
+    # train_dataset = ds_class(ds_path, split='train',      transform=ds_transform['train'], preload_data=train_opts.preloadData)
+    # valid_dataset = ds_class(ds_path, split='val',        transform=ds_transform['valid'], preload_data=train_opts.preloadData)
+    # test_dataset  = ds_class(ds_path, split='test',       transform=ds_transform['valid'], preload_data=train_opts.preloadData)
     
-    train_loader = DataLoader(dataset=train_dataset, num_workers=0, batch_size=train_opts.batchSize, shuffle=True)
-    valid_loader = DataLoader(dataset=valid_dataset, num_workers=0, batch_size=train_opts.batchSize, shuffle=False)
-    test_loader  = DataLoader(dataset=test_dataset,  num_workers=0, batch_size=train_opts.batchSize, shuffle=False)
+    # train_loader = DataLoader(dataset=train_dataset, num_workers=0, batch_size=train_opts.batchSize, shuffle=True)
+    # valid_loader = DataLoader(dataset=valid_dataset, num_workers=0, batch_size=train_opts.batchSize, shuffle=False)
+    # test_loader  = DataLoader(dataset=test_dataset,  num_workers=0, batch_size=train_opts.batchSize, shuffle=False)
+
+    train_transform = get_cityscapes_transforms()
+    valid_transform = get_cityscapes_transforms()
+
+    train_dataset = get_cityscapes_dataset(data_path='data', split='train', transform=train_transform)
+    valid_dataset = get_cityscapes_dataset(data_path='data', split='val', transform=valid_transform)
+    test_dataset = get_cityscapes_dataset(data_path='data', split='test', transform=valid_transform)
+
+    train_loader = DataLoader(dataset=train_dataset, num_workers=4, batch_size=train_opts.batchSize, shuffle=True)
+    valid_loader = DataLoader(dataset=valid_dataset, num_workers=4, batch_size=train_opts.batchSize, shuffle=False)
+    test_loader = DataLoader(dataset=test_dataset, num_workers=4, batch_size=train_opts.batchSize, shuffle=False)
+
 
     # # Get the first image from the dataset
     first_img = train_loader.dataset[0]
@@ -55,7 +88,7 @@ def train(arguments):
     # plt.axis('off')  # Turn off axis
     # plt.show()
 
-    return
+    # return
 
     # Visualisation Parameters
     visualizer = Visualiser(json_opts.visualisation, save_dir=model.save_dir)
